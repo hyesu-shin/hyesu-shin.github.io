4b09c0d82771c3832b2d61ef066cf478ebe9f81a
hshello <hehello@cloud-stones.com>
2021-07-01 21:08:55 +0900

    Detached Component

--- src/app.js
+++ src/app.js
@@ -1,10 +1,25 @@
+import Component from "./core/Component.js";
 import Items from "./components/Items.js";
+import Header from "./components/Header.js";
 
-class App {
-    constructor() {
-        const $app = document.querySelector('#app');
-        new Items($app);
+export default class App extends Component {
+    setup() {
+        this.$state = { items: ['item1', 'item2'] };
+    }
+
+    template() {
+        const { items } = this.$state;
+        return `
+            <div class="header"></div>
+            <div class="items"></div>
+        `
+    }
+
+    mounted() {
+        const $header = this.$target.getElementsByClassName('header');
+        const $items = this.$target.getElementsByClassName('items');
+
+        new Header($header, {});
+        new Items($items, {});
     }
 }
-
-new App();
--- src/core/Component.js
+++ src/core/Component.js
@@ -1,23 +1,40 @@
 export default class Component {
     $target;
+    $props;
     $state;
 
-    constructor($target) {
+    constructor($target, $props) {
         this.$target = $target;
+        this.$props = $props;   // $props 할당
         this.setup();
+        this.setEvent();
         this.render();
     }
 
     setup () {}
+    mounted () {}
     template () { return ''; }
     render () {
         this.$target.innerHTML = this.template();
-        this.setEvent();
+        this.mounted();
     }
 
     setEvent () {}
     setState (newState) {
         this.$state = { ...this.$state, ...newState };
         this.render();
+    }
+
+    // 이벤트 등록 함수 추가
+    addEvent (eventType, selector, callback) {
+        const children = [ ...this.$target.querySelectorAll(selector) ];
+        // selector 에 명시한 것 보다 더 하위 요소가 선택되는 경우가 있을 땐
+        // closest 를 이용하여 처리
+        const isTarget = (target) => children.includes(target) || target.closest(selector);
+
+        this.$target.addEventListener(eventType, event => {
+            if(!isTarget(event.target)) return false;
+            callback(event);
+        })
     }
 }
