565f522a446fed52f932ede5cad6039c90e6c830
hyesu <peche3@naver.com>
2021-09-16 00:59:15 +0900

    init firebase server

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+// import firebase from "firebase";
+//
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAx0V9P4y2a327QX5a-IHyliC2zJfEcfsM",
+//     authDomain: "githubpage-4ebaf.firebaseapp.com",
+//     databaseURL: "https://githubpage-4ebaf-default-rtdb.asia-southeast1.firebasedatabase.app",
+//     projectId: "githubpage-4ebaf",
+//     storageBucket: "githubpage-4ebaf.appspot.com",
+//     messagingSenderId: "299075645558",
+//     appId: "1:299075645558:web:988c8f6ea1081d72b16f57"
+// };
+//
+// const app = firebase.initializeApp(firebaseConfig);
--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,23 @@
 const path = require('path');
 
 module.exports = {
+    devtool: 'eval',
     devServer: {
         // 404 error -> index.html 로 리다이렉트
         historyApiFallback: true
     },
     entry : {
         router: './src/core/Router.js',
-        main : './src/app.js'
+        main : './src/main.js'
     },
     output: {
        path : path.resolve('./dist'),
        filename : '[name].js'
+    },
+    // node_modules 의 모듈들을 인식할 수 있음
+    // extensions 에 입력된 확장자의 경우 webpack 에서 인식해주기 때문에 입력하지 않아도 됨
+    resolve: {
+        modules: ['./node_modules'],
+        extensions: ['.js', '.css'],
     }
 }
