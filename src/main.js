ad54233bd75c760931f10308e8428bc4d4dc447b
hshello <hehello@cloud-stones.com>
2021-10-01 11:49:21 +0900

    Build project (bundle.js)

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // Entry Point
 
 // async/await 사용을 위한 babel/polyfill import
-import "@babel/polyfill"
+// import "@babel/polyfill"
 import App from './app.js'
 // import {initialRoutes} from "./core/Router";
 
